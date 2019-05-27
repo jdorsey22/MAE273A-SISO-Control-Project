0c60527bb9367c00a7149eaf3d97ad1322b59288
felipe valdez <fdvaldez@ucdavis.edu>
2019-05-27 16:18:59 -0700

    more files

--- BatteryModel_1stOrder.m
+++ BatteryModel_1stOrder.m
@@ -16,13 +16,8 @@ A = [-1/(Rc*Cc) 0; 0 0] ;
 B = [1/Cc; -1/Cbat];
 C = [-1 alpha];
 D = -R0;
-SI = [s 0;0 s];
-% SI_inv = (SI - A)^-1
-% C_SIinv = C*SI_inv
-% C_SIinv_B =C_SIinv*B
-% final= C_SIinv_B+D
-% Gp = -R0 - Rc/(Rc*Cc*s+1) - alpha/(Cbat*s) %hand computed
 
+SI = [s 0;0 s];
 Gp = C*(SI-A)^-1*B+D; %plant
 Y = K*wn^2*s/(s^2+2*zeta*wn*s+wn^2); %youla
 T = minreal(Gp*Y); %complimentary
@@ -35,4 +30,3 @@ figure(1)
 bode(Y,T,S), legend('Y','T','S')
 figure(2)
 step(sysTF), stepinfo(sysTF)
-
